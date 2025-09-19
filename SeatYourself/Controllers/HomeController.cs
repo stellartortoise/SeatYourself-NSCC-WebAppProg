ce1449919a732ebfccf718ba22f129bef662e4fe
apb-nscc <the.alchemist.coder@gmail.com>
2025-09-19 09:06:26 -0300

    Added Event Class

--- SeatYourself/Controllers/HomeController.cs
+++ SeatYourself/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SeatYourself.Controllers
             return View();
         }
 
+        public IActionResult Event()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ SeatYourself/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace SeatYourself.Models
+{
+    public class Event
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+        public string Location { get; set; }
+        public string Owner { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Available Seats will to here at some point
+    }
+}
