e442a5ad887e0c8ddca2d7528c4c16ffeec2468e
apb-nscc <the.alchemist.coder@gmail.com>
2025-09-30 13:41:12 -0300

    Migration Complete, Database Recreated

---
+++ SeatYourself/Migrations/20250930163051_UpdateOccasionsClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeatYourself.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOccasionsClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OccasionTime",
+                table: "Occasion",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Occasion",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Venue",
+                table: "Occasion",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Venue",
+                table: "Occasion");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OccasionTime",
+                table: "Occasion",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Occasion",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
